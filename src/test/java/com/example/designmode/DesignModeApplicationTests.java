84c7a2f8e402e8ada27a46a4d4923e1ca633dd6c
杨程凯 <9290411+yang-chengkai@user.noreply.gitee.com>
2022-11-26 10:14:47 +0800

    one

---
+++ src/test/java/com/example/designmode/DesignModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
